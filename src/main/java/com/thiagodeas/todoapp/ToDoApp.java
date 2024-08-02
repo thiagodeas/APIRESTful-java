749376df1acac213a46b95d694fda2fd3e04f791
thiagodeas <sousaalvesth@gmail.com>
2024-08-02 12:18:46 -0300

    configurando conexão spring x mysql

---
+++ src/main/java/com/thiagodeas/todoapp/ToDoApp.java
@@ -0,0 +1,11 @@
+package com.thiagodeas.todoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoApp {
+    public static void main(String[] args) {
+        SpringApplication.run(ToDoApp.class, args);
+    }
+}
