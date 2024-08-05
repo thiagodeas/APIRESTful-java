6b8976a2491cdb5d1208176bd7a7c237c2103db7
thiagodeas <sousaalvesth@gmail.com>
2024-08-05 13:09:17 -0300

    criando UserServices

---
+++ src/main/java/com/thiagodeas/todoapp/services/UserServices.java
@@ -0,0 +1,50 @@
+package com.thiagodeas.todoapp.services;
+
+import com.thiagodeas.todoapp.models.User;
+import com.thiagodeas.todoapp.repositories.TaskRepository;
+import com.thiagodeas.todoapp.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+public class UserServices {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+    public User findById(Long id) {
+        Optional<User> user = this.userRepository.findById(id);
+        return user.orElseThrow(() -> new RuntimeException(
+                "Usuário não encontrado! Id: " + id + ", Tipo: " + User.class.getName()
+        ));
+    }
+
+    @Transactional
+    public User create(User obj) {
+        obj.setId(null);
+        obj = this.userRepository.save(obj);
+        return obj;
+    }
+
+    @Transactional
+    public User update(User obj) {
+        User newObj = findById(obj.getId());
+        newObj.setPassword(obj.getPassword());
+        return this.userRepository.save(newObj);
+    }
+
+    public void delete (Long id) {
+        findById(id);
+        try {
+            this.userRepository.deleteById(id);
+        } catch (Exception e) {
+            throw new RuntimeException("Não é possível excluir pois há entidades relacionadas!");
+        }
+    }
+}
