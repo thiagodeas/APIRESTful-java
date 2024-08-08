2c29dbb9075f9d3dce03ab194dc24c835e29fe24
thiagodeas <sousaalvesth@gmail.com>
2024-08-08 15:59:58 -0300

    adicionando password encoder e criando ProfileEnum

--- src/main/java/com/thiagodeas/todoapp/models/User.java
+++ src/main/java/com/thiagodeas/todoapp/models/User.java
@@ -1,14 +1,18 @@
 package com.thiagodeas.todoapp.models;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.thiagodeas.todoapp.models.enums.ProfileEnum;
 import lombok.*;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.util.HashSet;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Entity
 @Table(name = User.TABLE_NAME)
@@ -49,4 +53,18 @@ public class User {
     @JsonProperty(access= JsonProperty.Access.WRITE_ONLY)
     private List<Task> tasks = new ArrayList<Task>();
 
+    @ElementCollection(fetch = FetchType.EAGER)
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @CollectionTable(name = "user_profile")
+    @Column(name = "profile", nullable = false)
+    private Set<Integer> profiles = new HashSet<>();
+
+    public Set<ProfileEnum> getProfiles() {
+        return this.profiles.stream().map(x -> ProfileEnum.toEnum(x))
+                .collect(Collectors.toSet());
+    }
+
+    public void addProfile(ProfileEnum profileEnum){
+        this.profiles.add(profileEnum.getCode());
+    }
 }
